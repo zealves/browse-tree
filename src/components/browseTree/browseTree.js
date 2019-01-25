9219c1bb06c8b4c473c2a46209bcc01f9cd46b48
José Alves <josee_alves@sapo.pt>
2019-01-25 13:50:14 +0000

    Wip: add delete item

--- src/components/browseTree/browseTree.js
+++ src/components/browseTree/browseTree.js
@@ -5,8 +5,10 @@ import arrowLeft from '../../assets/img/arrow-left.png'
 import folder from '../../assets/img/folder.png'
 import folderClose from '../../assets/img/folderClose.png'
 import dot from '../../assets/img/dot.png'
+import plus from '../../assets/img/plus.png'
+import plusFolder from '../../assets/img/plusFolder.png'
 
-const Item = ({ data, index, renderChildren, columnPosition, selected, updateItem }) => {
+const Item = ({ data, index, renderChildren, columnPosition, selected, updateItem, deleteItem }) => {
     const { title, children, value } = data
     let classNames = selected === index && 'selected '
     if (children)  classNames += children.length > 0 && ' haveChildren'
@@ -15,7 +17,16 @@ const Item = ({ data, index, renderChildren, columnPosition, selected, updateIte
         <h1 data-pos={index}  className={classNames}>
             <span className='textContent'>
                 {children.length > 0 ? 
-                    (selected === index ? <img className={'folder'} alt={'#'} src={folder} /> : <img className={'folder'} alt={'#'} src={folderClose} />)
+                    (selected === index ?
+                        <img
+                            className={'folder'}
+                            alt={'#'}
+                            src={folder} />
+                        :
+                        <img
+                            className={'folder'}
+                            alt={'#'}
+                            src={folderClose} />)
                     : '' }
                 <input  
                     readOnly
@@ -24,12 +35,23 @@ const Item = ({ data, index, renderChildren, columnPosition, selected, updateIte
                     type='text'
                     value={title}
                     onChange={(e) => updateItem(e.target, columnPosition, index)} />
+                <span
+                    style={{ display: 'block' }}
+                    onClick={() => { if(window.confirm('Are you sure you want to delete this item?')) { deleteItem(columnPosition, index)}}}>
+                    delete
+                </span>
             </span>
             <div className={'renderChildren'} onClick={() => (value ? renderChildren(index, columnPosition, value) : renderChildren(index, columnPosition))}>
                 {children.length > 0 ?
-                    <img className={'arrow'} alt={'>'} src={arrowRight}
+                    <img
+                        className={'arrow'}
+                        alt={'>'}
+                        src={arrowRight}
                 /> :
-                    <img className={'arrow'} alt={'.'} src={dot} />
+                    <img
+                        className={'arrow'}
+                        alt={'.'}
+                        src={dot} />
                 }
             </div>
         </h1>
@@ -196,8 +218,109 @@ class BrowseTree extends Component {
         this.setState({ path, childrenElements, elements: elements_ })
     }
 
+    addItem = (columnPosition, folder) => {
+        let childrenElements = this.state.childrenElements
+        let elements_ = this.state.elements
+        let currentPath = {}
+
+        this.state.selectedElements.forEach((v, k) => {
+            if (k === 0 && elements_[v]) {
+                if (k === columnPosition) {
+                    currentPath = elements_
+                    let children = []
+                    let title = 'New Item'
+                    if (folder) {
+                        children.push({ title: 'New item', children: [] })
+                        title = 'New folder'
+                    }
+                    const newItem = { title, children }
+                    currentPath.push(newItem)
+                    return
+                }
+                else {
+                    currentPath = elements_[v]
+                }
+            }
+            else if (k <= columnPosition) {
+                if (columnPosition === k) {
+                    currentPath = currentPath.children
+                    let children = []
+                    let title = 'New Item'
+                    if (folder) {
+                        children.push({ title: 'New item', children: [] })
+                        title = 'New folder'
+                    }
+                    const newItem = { title, children }
+                    currentPath.push(newItem)
+                    return
+                }
+                else {
+                    currentPath = currentPath.children[v]
+                    if (k === this.state.selectedElements.length - 1) {
+                        currentPath = currentPath.children
+                        let children = []
+                        let title = 'New Item'
+                        if (folder) {
+                            children.push({ title: 'New item', children: [] })
+                            title = 'New folder'
+                        }
+                        const newItem = { title, children }
+                        currentPath.push(newItem)
+                        return
+                    }
+                }
+            }
+        })
+
+        const path = this.getPath(0, this.state.selectedElements, elements_, [], [])
+        this.setState({ path, childrenElements, elements: elements_ })
+    }
+
+    deleteItem = (columnPosition, index) => {
+        let childrenElements = this.state.childrenElements
+        let elements_ = this.state.elements
+        let currentPath = {}
+
+        this.state.selectedElements.forEach((v, k) => {
+            if (k === 0 && elements_[v]) {
+                if (k === columnPosition) {
+                    currentPath = elements_
+                    currentPath.splice(index, 1)
+                    return
+                }
+                else {
+                    currentPath = elements_[v]
+                }
+            }
+            else if (k <= columnPosition) {
+                if (columnPosition === k) {
+                    if (currentPath && currentPath.children)
+                        currentPath.children.splice(index, 1)
+                    return
+                }
+                else {
+                    currentPath = currentPath.children[v]
+                    if (currentPath && k === this.state.selectedElements.length - 1) {
+                        currentPath.children.splice(index, 1)
+                        return
+                    }
+                }
+            }
+        })
+
+        /* regenerate tree */
+        const data = this.generateNodes(this.state.selectedElements, elements_, 0)
+        childrenElements = data.elements
+        const selectedElements = data.selected
+        /*  /regenerate tree */
+
+        const path = this.getPath(0, selectedElements, elements_, [], [])
+        this.setState({ path, childrenElements, elements: elements_ })
+    }
+
     getPath = (pos, selected, list, result, path) => {
         let res = result
+
         if (typeof(selected[pos]) !== undefined) {
             const index = selected[pos]
             if (pos === 0) {
@@ -255,8 +378,12 @@ class BrowseTree extends Component {
                         )}
                         {childrenElements.map((item, pos) => (
                             <div style={this.columnStyle} className={item.hidden ? 'columnHidden' : (item.length > 0 ? 'column' : '')} key={pos}>
+                                <div className={'interactionIcons'}>
+                                    <img alt={'Add single item'} src={plus} className={'addItem'} onClick={() => this.addItem(pos, false)} />
+                                    <img alt={'Add folder item'} src={plusFolder} className={'addFolderItem'} onClick={() => this.addItem(pos, true)} />
+                                </div>
                                 {item.map((e,i) =>
-                                    <Item updateItem={this.updateItem} selected={selectedElements[pos]} columnPosition={pos} data={e} key={i} index={i} renderChildren={this.renderChildren} />
+                                    <Item deleteItem={this.deleteItem} updateItem={this.updateItem} selected={selectedElements[pos]} columnPosition={pos} data={e} key={i} index={i} renderChildren={this.renderChildren} />
                                 )}
                             </div>
                         ))}
